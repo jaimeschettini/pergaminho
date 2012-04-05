1a1f40bae75adc39020f8b25ccab5a0fd9768188
Jaime Schettini <jaime.schettini@gmail.com>
2012-04-05 20:45:56 -0300

    just before generating layout using bootstrap

--- app/models/document.rb
+++ app/models/document.rb
@@ -12,40 +12,39 @@ class Document < ActiveRecord::Base
 	end
   
  	def search
- 		puts "jaimeeeeeeee"
- 		puts description.blank?
 	    Document.find(:all, :conditions => conditions)
 	end
 
 	def description_conditions
-		puts "description"
 		["description LIKE ?", "%#{description}%"] unless description.blank?
 	end
 
 	def issue_conditions
-		puts "issue"
-		["description LIKE ?", "%#{description}%"] unless description.blank?
+		["issue_id = ?", "#{issue.id}"] unless issue.blank? || issue.id.blank?
 	end
 
+	def company_conditions
+		["company_id = ?", "#{company.id}"] unless company.blank? || company.id.blank?
+	end
+
+	def department_conditions
+		["department_id = ?", "#{department.id}"] unless department.blank? || department.id.blank?
+	end
+
+
 	def conditions
-		puts "11111"
 	  [conditions_clauses.join(' AND '), *conditions_options]
 	end
 
 	def conditions_clauses
-		puts "22222"
 	  conditions_parts.map { |condition| condition.first }
 	end
 
 	def conditions_options
-		puts "3333"
 	  conditions_parts.map { |condition| condition[1..-1] }.flatten
 	end
 
 	def conditions_parts
-		puts "4444"
-		puts methods.grep(/_conditions$/).length
-		puts "44445555555"
 	  methods.grep(/_conditions$/).map { |m| send(m) }.compact
 	end
 end
